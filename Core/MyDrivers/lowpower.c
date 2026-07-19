d2b9a6dc115193c3386cf323ad7034a37f5711cf
ppul05 <pablo.pul2005@gmail.com>
2026-07-19 18:22:26 +0200

    Initialization of low power mode

--- Core/MyDrivers/lowpower.c
+++ Core/MyDrivers/lowpower.c
@@ -5,4 +5,22 @@
  *      Author: PABLO
  */
 
+#include "stm32l1xx.h"
+#include "lowpower.h"
 
+void LOWPOWER_EnterStopMode(void){
+
+	// PWR clock enabled in rtc.c
+
+	PWR->CR |= PWR_CR_LPSDSR;
+	PWR->CR &= ~PWR_CR_PDDS;
+
+	PWR->CSR |= PWR_CSR_WUF;
+
+	SCB->SCR |= SCB_SCR_SLEEPDEEP_Msk;  // SLEEPDEEP = 1 (Stop mode)
+
+	__WFI();
+
+	//SystemClock_Config();
+
+}
