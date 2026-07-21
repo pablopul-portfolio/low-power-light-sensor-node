0aa37f941fa7174ce980ebbe6fd9f12313026bca
ppul05 <pablo.pul2005@gmail.com>
2026-07-21 13:04:51 +0200

    Updated

--- Core/MyDrivers/lowpower.c
+++ Core/MyDrivers/lowpower.c
@@ -15,7 +15,9 @@ void LOWPOWER_EnterStopMode(void){
 
 	// PWR clock enabled in rtc.c
 
-	//DBGMCU->CR |= DBGMCU_CR_DBG_STOP;    // This was added due to issues connecting to debugger
+	PWR->CR |= PWR_CR_DBP;  // Backup domain
+
+	DBGMCU->CR |= DBGMCU_CR_DBG_STOP;    // This was added due to issues connecting to debugger
 
 	PWR->CR |= PWR_CR_LPSDSR;
 	PWR->CR &= ~PWR_CR_PDDS;
@@ -25,7 +27,11 @@ void LOWPOWER_EnterStopMode(void){
 	SCB->SCR |= SCB_SCR_SLEEPDEEP_Msk;  // SLEEPDEEP = 1 (Stop mode)
 
 	HAL_SuspendTick();  // Some interrupts may occur here and make it not enter the stop mode
+
+	__DSB();
 	__WFI();
+
+	HAL_ResumeTick();
 
 	//GPIOA->ODR ^= (1 << 1);
 
